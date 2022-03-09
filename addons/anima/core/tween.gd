0882f912df247f75ef5dc984beb0fda639d06972
Alessandro Senese <alessandro.senese@formidable.com>
2022-03-09 09:15:44 +0000

    Fix easing

--- addons/anima/core/tween.gd
+++ addons/anima/core/tween.gd
@@ -688,20 +688,14 @@ class AnimatedItem extends Node:
 		animate(elapsed)
 
 	func _cubic_bezier(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2, t: float) -> float:
-#		var a = Vector2.ZERO.cubic_interpolate(p1, p2, p3, t)
-#
-#		print(a.abs())
-#		return a.length_squared()
-		var q0 = p0.lerp(p1, t)
-		var q1 = p1.lerp(p2, t)
-		var q2 = p2.lerp(p3, t)
+		var q0 := p0.lerp(p1, t)
+		var q1 := p1.lerp(p2, t)
+		var q2 := p2.lerp(p3, t)
 
-		var r0 = q0.lerp(q1, t)
-		var r1 = q1.lerp(q2, t)
+		var r0 := q0.lerp(q1, t)
+		var r1 := q1.lerp(q2, t)
 
-		var s = r0.lerp(r1, t)
-
-		return s
+		return r0.lerp(r1, t).y
 
 	func _execute_callback(callback) -> void:
 		var fn: Callable
